9bcee5e7a6406e6434ed7b155a24e36464f111a2
Mao <code@mao.to>
2020-08-20 00:55:52 +0800

    clean codes

--- config.sh
+++ config.sh
@@ -3,8 +3,8 @@
 root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && cd .. && pwd)
 
 # install sarasa fonts
-fc-list 2>&1 | grep -q "Sarasa"
-if [[ $? != 0 ]]; then
+
+if fc-list 2>&1 | grep -q "Sarasa"; then
 	cp -rvf "$root_dir/macos/optional/fonts/*.ttf" ~/Library/Fonts
 	fc-cache -f -v ~/Library/Fonts
 fi
