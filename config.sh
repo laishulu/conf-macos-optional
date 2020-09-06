fee0b60c0fd6adfa7faa0dbcfad8b1572b853b27
Mao <code@mao.to>
2020-09-06 09:04:01 +0800

    replace zsh with bash

--- config.sh
+++ config.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
 root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && cd .. && pwd)
 
@@ -13,13 +13,9 @@ if [[ $overwrite == "yes" ]]; then
 	open "$root_dir/macos/optional/Monokai-pro.terminal"
 fi
 
-if [[ $overwrite == "yes" || ! -e ~/.alacritty.yml ]]; then
-	ln -sf $root_dir/macos/optional/alacritty.yml $HOME/.alacritty.yml
-fi
-
 if [[ $macos_keymap = "switch" ]]; then
 	brew cask install karabiner-elements
-	if [[ ! -e ~/.config/karabiner/karabiner.json ]]; then
+	if ! [[ -e ~/.config/karabiner/karabiner.json ]]; then
 		mkdir -p ~/.config/karabiner
 		ln -sf $root_dir/macos/optional/karabiner.json ~/.config/karabiner/karabiner.json
 	fi
