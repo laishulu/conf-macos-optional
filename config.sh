d7bfe7d205bb788331791a3b899df532fbe3c340
Mao <code@mao.to>
2020-09-29 00:13:19 +0800

    remove fonts

--- config.sh
+++ config.sh
@@ -2,13 +2,6 @@
 
 root_dir=$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd .. && cd .. && pwd)
 
-# install sarasa fonts
-
-if fc-list 2>&1 | grep -q "Sarasa"; then
-	cp -rvf "$root_dir/macos/optional/fonts/*.ttf" ~/Library/Fonts
-	fc-cache -f -v ~/Library/Fonts
-fi
-
 if [[ $overwrite == "yes" ]]; then
 	open "$root_dir/macos/optional/Monokai-pro.terminal"
 fi
